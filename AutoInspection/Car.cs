dfcf0a99ce7d377de8bdd5b943477ea93d4e5efd
DESKTOP-EGIG8AN\Student <cestlavie20180801@gmail.com>
2022-09-24 22:34:09 +0200

    add project

---
+++ AutoInspection/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoInspection
+{
+    internal class Car
+    {
+        public string carName = "Audi";
+        public int baujahr = 1995;
+        public int lastInspectionYear = 2010;
+
+        // Konstruktor
+        public Car(string newName, int newBaujahr)
+        {
+            carName = newName;
+            baujahr = newBaujahr;
+        }
+
+        // Methode
+        public void Inspect()
+        {
+            lastInspectionYear = 2020;
+        }
+    }
+}
---
+++ AutoInspection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AutoInspection
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            // Array von Autos
+            Car[] cars = new Car[6];
+            cars[0] = new Car("Mercedes G", 2008);
+            cars[1] = new Car("BMW 3", 2001);
+            cars[2] = new Car("Audi A6", 2005);
+            cars[3] = new Car("Audi A8", 2012);
+            cars[4] = new Car("Hyundai", 2003);
+            cars[5] = new Car("Toyota", 1995);
+
+
+            Console.WriteLine("Bitte geben Sie einen Suchbegriff ein:");
+
+            string searchTerm = Console.ReadLine();
+
+            Console.WriteLine("Suchbegriff ist: " + searchTerm);
+
+            Console.WriteLine("Folgende Autos wurden gefunden: " + searchTerm);
+
+            foreach(Car car in cars)
+            {
+                if (car.carName.Contains(searchTerm))   // Contains -> schaut nach Text der im Strig enthalten ist 
+                {
+                    Console.WriteLine(car.carName + "Baujahr: " + car.baujahr + " Letzte Inspektion: " + car.lastInspectionYear);
+                    car.Inspect();
+                    Console.WriteLine("Neues Inspektionsjahr: " + car.lastInspectionYear);
+                }
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            // Wiederaufruf von Main -> Programm wird wieder gestartet
+            Main();
+        }
+    }
+}
